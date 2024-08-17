1661916acbb88c9ab56f27865eda7051504d80e0
seungdangdang <yseungun.21@gmail.com>
2024-08-17 20:12:32 +0900

    feat: 설정 클래스 스캔을 위한 ComponentScan 경로 추가

--- core/src/main/java/com/quickdeal/core/CoreApplication.java
+++ core/src/main/java/com/quickdeal/core/CoreApplication.java
@@ -1,17 +1,21 @@
 package com.quickdeal.core;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.quickdeal.common")
-@ComponentScan("com.quickdeal.order")
-@ComponentScan("com.quickdeal.payment")
-@ComponentScan("com.quickdeal.product")
-@ComponentScan("com.quickdeal.user")
+@ComponentScan({
+    "com.quickdeal.common",
+    "com.quickdeal.order",
+    "com.quickdeal.payment",
+    "com.quickdeal.product",
+    "com.quickdeal.user",
+    "com.quickdeal.core.config"
+})
 public class CoreApplication {
 
   public static void main(String[] args) {
-
+    SpringApplication.run(CoreApplication.class, args);
   }
 }
