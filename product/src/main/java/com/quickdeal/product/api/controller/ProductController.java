ea9b44befe42ea7887f37f0c81966a3e04332d3f
seungdangdang <yseungun.21@gmail.com>
2024-08-18 00:24:05 +0900

    feat: 상품 디테일 반환 API 구현

--- product/src/main/java/com/quickdeal/product/api/controller/ProductController.java
+++ product/src/main/java/com/quickdeal/product/api/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.quickdeal.product.api.controller;
 
+import com.quickdeal.product.api.resource.ProductDetailResource;
 import com.quickdeal.product.api.resource.ProductListResource;
 import com.quickdeal.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class ProductController {
   public ProductListResource getProducts(@RequestParam(value = "lastId", required = false) Long lastId,
       Pageable pageable) {
     return productService.getProductList(lastId, pageable);
+  }
+
+  @GetMapping("detail/{productId}")
+  public ProductDetailResource getProductDetail(@PathVariable Long productId) {
+    return productService.getProductDetail(productId);
   }
 }
