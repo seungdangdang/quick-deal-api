2e63f7673d0dc79472ae9233b6d443751d223dc1
seungdangdang <yseungun.21@gmail.com>
2024-08-17 19:34:16 +0900

    feat: 상품, 카테고리 엔티티 및 카테고리 타입 이넘 생성

---
+++ product/src/main/java/com/quickdeal/product/infrastructure/entity/Category.java
@@ -0,0 +1,36 @@
+package com.quickdeal.product.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private CategoryType name;
+
+  @OneToMany(mappedBy = "category")
+  private List<Product> products;
+
+  public CategoryType getCategoryType() {
+    return this.name;
+  }
+}
