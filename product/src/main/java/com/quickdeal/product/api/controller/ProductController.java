b644021222df54f5c58abc5a0f243c922f0850ab
seungdangdang <yseungun.21@gmail.com>
2024-08-17 19:40:10 +0900

    feat: 상품 리스트 반환 API 구현

---
+++ product/src/main/java/com/quickdeal/product/api/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.quickdeal.product.api.controller;
+
+import com.quickdeal.product.api.resource.ProductListResource;
+import com.quickdeal.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+  private final ProductService productService;
+
+  @Autowired
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GetMapping("/products")
+  public ProductListResource getProducts(@RequestParam(value = "lastId", required = false) Long lastId,
+      Pageable pageable) {
+    return productService.getProductList(lastId, pageable);
+  }
+}
