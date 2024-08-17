70e964b19c873f8c485a713b889014c4694ea78a
seungdangdang <yseungun.21@gmail.com>
2024-08-17 19:38:25 +0900

    feat: 상품 리스트 조회 서비스 메서드 구현

---
+++ product/src/main/java/com/quickdeal/product/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.quickdeal.product.service;
+
+import com.quickdeal.product.api.resource.ProductListResource;
+import com.quickdeal.product.infrastructure.entity.Product;
+import com.quickdeal.product.infrastructure.repository.ProductRepository;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+  private final ProductRepository productRepository;
+
+  @Autowired
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public ProductListResource getProductList(Long lastId, Pageable pageable) {
+    Slice<Product> productSlice = productRepository.findByCursor(lastId, pageable);
+    var productList = productSlice.getContent()
+        .stream()
+        .map(this::toProductRecord)
+        .collect(Collectors.toList());
+
+    return new ProductListResource(productList, productSlice.hasNext());
+  }
+
+  private ProductRecord toProductRecord(Product product) {
+    return new ProductRecord(
+        product.getId(),
+        product.getCategory().getCategoryType(),
+        product.getName(),
+        product.getDescription(),
+        product.getPrice(),
+        product.getStockQuantity(),
+        product.getCreatedAt(),
+        product.getUpdatedAt()
+    );
+  }
+}
