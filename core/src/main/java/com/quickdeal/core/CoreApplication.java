72c6eb51516d152838a31dffffecedff1d09c797
seungdangdang <yseungun.21@gmail.com>
2024-08-20 15:07:27 +0900

    refactor: Core 모듈의 스캔 설정을 클래스 파일로 이동

--- core/src/main/java/com/quickdeal/core/CoreApplication.java
+++ core/src/main/java/com/quickdeal/core/CoreApplication.java
@@ -2,16 +2,24 @@ package com.quickdeal.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({
     "com.quickdeal.common",
     "com.quickdeal.order",
-    "com.quickdeal.payment",
     "com.quickdeal.product",
-    "com.quickdeal.user",
-    "com.quickdeal.core.config"
+    "com.quickdeal.user"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.quickdeal.product.infrastructure.repository",
+    "com.quickdeal.order.infrastructure.repository"
+})
+@EntityScan(basePackages = {
+    "com.quickdeal.product.infrastructure.entity",
+    "com.quickdeal.order.infrastructure.entity"
 })
 public class CoreApplication {
 
