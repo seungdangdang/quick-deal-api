3cd72a0e8bb1ca670dcc3376049f05fe2ac215e9
seungdangdang <yseungun.21@gmail.com>
2024-08-20 15:32:39 +0900

    feat: 레디스 및 JWT 시크릿 키 설정 파일 적용

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -2,6 +2,7 @@ class Versions {
     companion object {
         const val REDIS = "3.2.4"
         const val KAFKA = "3.7.0"
+        const val JJWT_API = "0.11.5"
     }
 }
 
@@ -17,9 +18,13 @@ repositories {
 }
 
 dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:${Versions.JJWT_API}")
     implementation("org.springframework.boot:spring-boot-starter-data-redis:${Versions.REDIS}")
     implementation("org.apache.kafka:kafka-clients:${Versions.KAFKA}")
     implementation(project(":common"))
+    implementation(project(":product"))
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${Versions.JJWT_API}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${Versions.JJWT_API}")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
