367db8582624e293965c43fd7372119ad6b1f00e
seungdangdang <yseungun.21@gmail.com>
2024-08-20 15:29:22 +0900

    feat: 대기열 토큰 발급 서비스 구현

---
+++ order/src/main/java/com/quickdeal/order/service/TokenService.java
@@ -0,0 +1,21 @@
+package com.quickdeal.order.service;
+
+import com.quickdeal.order.service.domain.QueueToken;
+import com.quickdeal.order.util.JWTUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+  private final JWTUtil jwtUtil;
+
+  public TokenService(JWTUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
+  }
+
+  // 대기열 토큰 생성 로직
+  public QueueToken generateQueueNumber(String userId, Long newQueueNumber) {
+    String token = jwtUtil.createQueueJwt(userId, newQueueNumber);
+    return new QueueToken(userId, newQueueNumber, token);
+  }
+}
