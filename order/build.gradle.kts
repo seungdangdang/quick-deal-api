5601e812d83e10b5a8f3c0b918ae3cc46d83571e
seungdangdang <yseungun.21@gmail.com>
2024-08-22 21:58:31 +0900

    feat: 카프카 설정 추가 및 카프카 브로커, UI 컨테이너 추가

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -2,6 +2,7 @@ class Versions {
     companion object {
         const val REDIS = "3.2.4"
         const val KAFKA = "3.7.0"
+        const val SPRING_KAFKA = "3.2.0"
         const val JJWT_API = "0.11.5"
     }
 }
@@ -20,6 +21,7 @@ repositories {
 dependencies {
     implementation("io.jsonwebtoken:jjwt-api:${Versions.JJWT_API}")
     implementation("org.springframework.boot:spring-boot-starter-data-redis:${Versions.REDIS}")
+    implementation("org.springframework.kafka:spring-kafka:${Versions.SPRING_KAFKA}")
     implementation("org.apache.kafka:kafka-clients:${Versions.KAFKA}")
     implementation(project(":common"))
     implementation(project(":product"))
