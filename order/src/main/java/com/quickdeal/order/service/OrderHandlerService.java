b9bc4b2ad78bb73668bb091785f2a90c62b17a19
seungdangdang <yseungun.21@gmail.com>
2024-08-22 22:12:53 +0900

    feat: 대기열 생성 시 카프카 토픽에 메시지 삽입 로직 추가

--- order/src/main/java/com/quickdeal/order/service/OrderHandlerService.java
+++ order/src/main/java/com/quickdeal/order/service/OrderHandlerService.java
@@ -1,6 +1,7 @@
 package com.quickdeal.order.service;
 
 import com.quickdeal.order.api.resource.QueueCommand;
+import com.quickdeal.order.domain.QueueMessage;
 import com.quickdeal.order.domain.QueueToken;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,13 @@ public class OrderHandlerService {
 
   private final QueueService queueService;
   private final TokenService tokenService;
+  private final MessageQueueService messageQueueService;
 
-  public OrderHandlerService(QueueService queueService, TokenService tokenService) {
+  public OrderHandlerService(QueueService queueService, TokenService tokenService,
+      MessageQueueService messageQueueService) {
     this.queueService = queueService;
     this.tokenService = tokenService;
+    this.messageQueueService = messageQueueService;
   }
 
   public QueueToken generateQueue(QueueCommand command) {
@@ -20,9 +24,13 @@ public class OrderHandlerService {
     Long newQueueNumber = queueService.getNewQueueNumber(command.productId());
 
     // 새로운 대기열 토큰 발급
-    QueueToken queueToken = tokenService.generateQueueNumber(command.productId(), command.userUUID(), newQueueNumber);
+    QueueToken queueToken = tokenService.generateQueueNumber(command.productId(),
+        command.userUUID(), newQueueNumber);
 
-    // TODO - queue producer 요청 삽입 기능
+    // queue 메시지 삽입
+    QueueMessage queueMessage = new QueueMessage(queueToken.queueNumber(), queueToken.productId(),
+        queueToken.userUUID());
+    messageQueueService.publishMessage("queue", queueMessage);
 
     return queueToken;
   }
