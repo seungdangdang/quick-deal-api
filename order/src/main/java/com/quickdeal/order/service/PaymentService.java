761b2978e560451aa5f5e7b7881bb481bca8f601
seungdangdang <yseungun.21@gmail.com>
2024-08-23 21:41:58 +0900

    feat: 결제 도중 에러 시 order, payment 상태 업데이트 서비스 구현

--- order/src/main/java/com/quickdeal/order/service/PaymentService.java
+++ order/src/main/java/com/quickdeal/order/service/PaymentService.java
@@ -1,11 +1,15 @@
 package com.quickdeal.order.service;
 
 import com.quickdeal.order.domain.OrderStatusUpdateCommand;
+import com.quickdeal.order.domain.PaymentCommand;
 import com.quickdeal.order.infrastructure.entity.OrderStatus;
 import com.quickdeal.order.infrastructure.entity.PaymentStatus;
 import com.quickdeal.order.infrastructure.repository.PaymentRepository;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PaymentService {
@@ -19,25 +23,29 @@ public class PaymentService {
     this.orderService = orderService;
   }
 
-  public void processCheckout(Long orderId) {
-    // todo - 실제 결제 로직 추가
+  public void checkout(PaymentCommand paymentCommand) {
 
-    // 결제가 완료 여부에 따라 상태업데이트
-    updateDoneOrderPayment(orderId);
-    // todo - 상태값 반환 필요
   }
 
-  private void updateDoneOrderPayment(Long orderId) {
-    // 주문 상태 업데이트 하기
+  @Transactional
+  public void endedCheckout(Long orderId) {
     orderService.updateOrderStatus(new OrderStatusUpdateCommand(orderId, OrderStatus.DONE));
-    // 결제 상태 업데이트 하기
-    paymentRepository.updateOrderPayment(orderId, LocalDateTime.now(), PaymentStatus.DONE);
+    Instant paymentDate = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant();
+    paymentRepository.updateOrderPayment(orderId, paymentDate, PaymentStatus.DONE);
     // todo - 상태값 반환 필요
   }
 
+  @Transactional
   public void cancelCheckout(Long orderId) {
     orderService.updateOrderStatus(new OrderStatusUpdateCommand(orderId, OrderStatus.CANCEL));
     paymentRepository.updateOrderPayment(orderId, null, PaymentStatus.CANCEL);
+    // todo - 상태값 반환 필요
+  }
+
+  @Transactional
+  public void errorCheckout(Long orderId) {
+    orderService.updateOrderStatus(new OrderStatusUpdateCommand(orderId, OrderStatus.ERROR));
+    paymentRepository.updateOrderPayment(orderId, null, PaymentStatus.ERROR);
     // todo - 상태값 반환 필요
   }
 }
