206ec784266a43d0feb48411ad23a08c6fc3dd6b
seungdangdang <yseungun.21@gmail.com>
2024-08-23 17:38:02 +0900

    feat: 대기열큐 메시지 처리 서비스 구현 및 대기열 토큰 검증 순서 개선

--- order/src/main/java/com/quickdeal/order/service/QueueService.java
+++ order/src/main/java/com/quickdeal/order/service/QueueService.java
@@ -1,7 +1,9 @@
 package com.quickdeal.order.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quickdeal.order.api.resource.QueuePollingCommand;
 import com.quickdeal.order.config.RedisConfig;
+import com.quickdeal.order.domain.QueueMessage;
 import com.quickdeal.order.domain.QueueStatus;
 import io.jsonwebtoken.Claims;
 import java.util.Date;
@@ -14,31 +16,35 @@ public class QueueService {
   private final RedisTemplate<String, Long> redisTemplate;
   private final TokenService tokenService;
 
+  private static final int MAX_PAYMENT_PAGE_USERS = 10;
+  private final ObjectMapper objectMapper;
+
   public QueueService(RedisTemplate<String, Long> redisTemplate,
-      TokenService tokenService) {
+      TokenService tokenService, ObjectMapper objectMapper) {
     this.redisTemplate = redisTemplate;
     this.tokenService = tokenService;
+    this.objectMapper = objectMapper;
   }
 
   // 레디스에서 마지막으로 요청 들어온 대기번호 업데이트하는 코드
   public Long getNewQueueNumber(Long productId) {
     String key = RedisConfig.getLastQueueNumberKey(productId);
-    Long newQueueNumber = redisTemplate.opsForValue().increment(key, 1);
-    return newQueueNumber != null ? newQueueNumber : 1L;
+    Object newQueueNumber = redisTemplate.opsForValue().increment(key, 1);
+    return newQueueNumber != null ? Long.parseLong(String.valueOf(newQueueNumber)) : 1L;
   }
 
   // 레디스에서 마지막 요청 들어온 대기번호 가져오는 코드
   public Long getLastQueueNumber(Long productId) {
     String key = RedisConfig.getLastExitedQueueNumberKey(productId);
-    Long value = redisTemplate.opsForValue().get(key);
-    return value != null ? value : 0L;
+    Object value = redisTemplate.opsForValue().get(key);
+    return value != null ? Long.parseLong(String.valueOf(value)) : 0L;
   }
 
   // 레디스에서 마지막으로 대기열에서 빠져나간 대기번호 가져오는 코드
   public Long getLastExitedQueueNumber(Long productId) {
     String key = RedisConfig.getLastExitedQueueNumberKey(productId);
-    Long value = redisTemplate.opsForValue().get(key);
-    return value != null ? value : 0L;
+    Object value = redisTemplate.opsForValue().get(key);
+    return value != null ? Long.parseLong(String.valueOf(value)) : 0L;
   }
 
   public void updateLastExitedQueueNumber(Long productId,
@@ -68,10 +74,11 @@ public class QueueService {
     Long requestQueueNumber = queuePollingCommand.queueNumber();
     long remainingInQueue = requestQueueNumber - lastExitedQueueNumber;
 
+    Claims claims = tokenService.getClaimsByToken(queuePollingCommand.jwtToken());
+
     if (remainingInQueue <= 0) {
       return new QueueStatus(true, 0L, null);
     } else {
-      Claims claims = tokenService.getClaimsByToken(queuePollingCommand.jwtToken());
       Date expiration = claims.getExpiration();
       long now = System.currentTimeMillis();
       long timeUntilExpiration = expiration.getTime() - now;
@@ -84,6 +91,26 @@ public class QueueService {
         return new QueueStatus(false, remainingInQueue,
             queuePollingCommand.jwtToken());
       }
+    }
+  }
+
+  public void processQueueMessage(String message) throws Exception {
+    // 메시지를 QueueMessage 객체로 변환
+    QueueMessage queueMessage = objectMapper.readValue(message, QueueMessage.class);
+    Long productId = queueMessage.productId();
+
+    // 현재 결제 페이지에 있는 사용자 수를 가져옴
+    Integer currentCount = getCurrentPaymentPageUserCount(productId);
+
+    // 사용자 수가 최대값보다 적은지 확인
+    if (currentCount < MAX_PAYMENT_PAGE_USERS) {
+      // 사용자 수 증가
+      incrementPaymentPageUserCount(productId);
+      // 마지막으로 대기열을 나간 사용자 번호 업데이트
+      updateLastExitedQueueNumber(productId, queueMessage.queueNumber());
+    } else {
+      // 최대 사용자 수를 초과하면 예외 발생 todo - 커스텀 예외 적용
+      throw new Exception();
     }
   }
 }
