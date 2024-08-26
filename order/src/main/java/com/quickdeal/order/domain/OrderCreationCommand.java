cf373022118c1ee7fccb60c89b721c28e497f8ad
seungdangdang <yseungun.21@gmail.com>
2024-08-27 00:22:24 +0900

    chore: 주문 생성 시 수량 제한 로직 추가

--- order/src/main/java/com/quickdeal/order/domain/OrderCreationCommand.java
+++ order/src/main/java/com/quickdeal/order/domain/OrderCreationCommand.java
@@ -1,8 +1,15 @@
 package com.quickdeal.order.domain;
 
+import com.quickdeal.common.exception.BusinessRuleViolation;
+
 public record OrderCreationCommand(
     String userUUID,
     QuantityPerProduct quantityPerProduct
 ) {
 
+  public OrderCreationCommand {
+    if (quantityPerProduct.quantity() > 1) {
+      throw new BusinessRuleViolation("주문 1회 당 최대 주문 가능 수량인 1개를 초과했습니다.");
+    }
+  }
 }
