406d485ff6092edb3c1099c3c04045428ad443d1
seungdangdang <yseungun.21@gmail.com>
2024-09-04 10:55:30 +0900

    feat: CORS 설정 추가 - 프론트엔드 서버에서 접근 허용
    - http://localhost:3000

--- core/src/main/java/com/quickdeal/core/CoreApplication.java
+++ core/src/main/java/com/quickdeal/core/CoreApplication.java
@@ -3,8 +3,11 @@ package com.quickdeal.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan({
@@ -26,5 +29,20 @@ public class CoreApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CoreApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer CORSConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
   }
 }
