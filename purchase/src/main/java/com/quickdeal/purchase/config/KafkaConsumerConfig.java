660366278975540d034b5c5544b534b3fdc45682
seungdangdang <yseungun.21@gmail.com>
2024-09-07 13:35:47 +0900

    chore: 컨슈머의 적절한 토픽 주입을 위해 토픽 생성 메서드 생성
    - 현재 상품 개수인 1~40 번에 매칭되는 토픽을 생성하게끔 하여 컨슈머가 메시지를 받을 수 있음

--- purchase/src/main/java/com/quickdeal/purchase/config/KafkaConsumerConfig.java
+++ purchase/src/main/java/com/quickdeal/purchase/config/KafkaConsumerConfig.java
@@ -1,7 +1,10 @@
 package com.quickdeal.purchase.config;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,7 +28,7 @@ public class KafkaConsumerConfig {
     config.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-consumer-group");
     config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1); // 한 번에 하나의 메시지만 폴링
+    config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
     return new DefaultKafkaConsumerFactory<>(config);
   }
 
@@ -35,5 +38,12 @@ public class KafkaConsumerConfig {
     factory.setConsumerFactory(consumerFactory());
     factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
     return factory;
+  }
+
+  @Bean
+  public List<String> topicList() {
+    return IntStream.rangeClosed(1, 40)
+        .mapToObj(i -> "queue-" + i)
+        .collect(Collectors.toList());
   }
 }
--- purchase/src/main/java/com/quickdeal/purchase/service/KafkaQueueConsumer.java
+++ purchase/src/main/java/com/quickdeal/purchase/service/KafkaQueueConsumer.java
@@ -3,9 +3,11 @@ package com.quickdeal.purchase.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quickdeal.common.exception.MaxUserLimitExceededException;
+import com.quickdeal.common.exception.OrderStatusInvalidException;
 import com.quickdeal.purchase.domain.QueueMessage;
 import io.jsonwebtoken.Claims;
 import java.time.Duration;
+import java.util.List;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,38 +21,40 @@ public class KafkaQueueConsumer {
   private final Logger log;
   private final TokenService tokenService;
   private final TicketService queueService;
+  private final InMemoryService inMemoryService;
   private final ObjectMapper objectMapper;
+  private final List<String> topicList;
 
   public KafkaQueueConsumer(TokenService tokenService, TicketService queueService,
-      ObjectMapper objectMapper) {
+      InMemoryService inMemoryService, ObjectMapper objectMapper, List<String> topicList) {
     this.tokenService = tokenService;
     this.queueService = queueService;
+    this.inMemoryService = inMemoryService;
     this.objectMapper = objectMapper;
+    this.topicList = topicList;
     this.log = LoggerFactory.getLogger(this.getClass());
   }
 
-  @KafkaListener(topics = {"queue-1", "queue-2", "queue-3", "queue-4",
-      "queue-5", "queue-51", "queue-47"}, groupId = "payment-consumer-group")
-  public void consume(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
-    log.debug("[consume] message with offset: {}", record.offset());
+  @KafkaListener(topics = "#{@topicList}", groupId = "payment-consumer-group")
+  public void consume(ConsumerRecord<String, String> record, Acknowledgment acknowledgment)
+      throws JsonProcessingException {
+    QueueMessage queueMessage = objectMapper.readValue(record.value(), QueueMessage.class);
+    String token = queueMessage.ticketToken();
+    Claims claims = tokenService.validateTokenAndGetClaims(token);
+    log.debug("[consume] message with orderId: {} userId: {}", claims.get("order_id"),
+        queueMessage.userId());
     try {
-      QueueMessage queueMessage = objectMapper.readValue(record.value(), QueueMessage.class);
-      String token = queueMessage.ticketToken(); //TODO: ticketToken 은 나중에 삭제할 것
-      Claims claims = tokenService.validateTokenAndGetClaims(token); //TODO: ticketToken 은 나중에 삭제할 것
-      log.debug("[consume] message with orderId: {} userId: {}", claims.get("order_id"),
-          queueMessage.userId());
       queueService.validateTicketAndPaymentPageAccessible(queueMessage);
       acknowledgment.acknowledge();
       log.debug("[consume] offset committed successfully with orderId: {} userId: {}",
           claims.get("order_id"), queueMessage.userId());
     } catch (MaxUserLimitExceededException e) {
       acknowledgment.nack(Duration.ofMillis(500));
-    } catch (JsonProcessingException e) {
-      // todo - 예외처리
+    } catch (OrderStatusInvalidException e) {
+      inMemoryService.updateLastExitedTicketNumber(claims.get("product_id", Long.class),
+          claims.get("ticketNumber", Long.class));
     } catch (InterruptedException e) {
-      // todo - 예외처리
-    } catch (Exception e) {
-      // todo - 예외처리
+      throw new RuntimeException(e);
     }
   }
 }
