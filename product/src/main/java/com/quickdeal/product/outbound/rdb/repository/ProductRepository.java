20d8a461f8200f3dbfff7958609845851eaf9440
seungdangdang <yseungun.21@gmail.com>
2024-11-14 16:01:32 +0900

    refactor: 컨슘 시 재고 확인 후 재고가 없을 시 로직 종료
    TODO: 처리 중단 후 처리 로직 필요

--- product/src/main/java/com/quickdeal/product/outbound/rdb/repository/ProductRepository.java
+++ product/src/main/java/com/quickdeal/product/outbound/rdb/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.quickdeal.product.outbound.rdb.repository;
 
 import com.quickdeal.product.outbound.rdb.model.ProductEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,9 +14,12 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
   List<ProductEntity> findTop20ByIdLessThanOrderByIdDesc(Long id);
 
+  @Query("SELECT p.stockQuantity FROM Product p WHERE p.id = :id")
+  Optional<Long> findStockQuantityById(@Param("id") Long id);
+
   @Modifying
-  @Query("UPDATE Product p SET p.stockQuantity = p.stockQuantity - 1 WHERE p.id = :productId AND p.stockQuantity > 0")
-  void decreaseStockQuantity(@Param("productId") Long productId);
+  @Query("UPDATE Product p SET p.stockQuantity = p.stockQuantity - :quantity WHERE p.id = :productId")
+  void decreaseStockQuantityById(@Param("productId") Long productId, @Param("quantity") int quantity);
 
   @Modifying
   @Query("UPDATE Product p SET p.stockQuantity = p.stockQuantity + 1 WHERE p.id = :productId AND p.stockQuantity > 0")
